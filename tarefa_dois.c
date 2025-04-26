8f5fefee9752a40a3632f45da3d70872ab143b97
Manoel <manoelivisson4@hotmail.com>
2025-04-26 14:28:49 -0300

    Escrevi uma mesnsagem genérica na tela do OLED

--- tarefa_dois.c
+++ tarefa_dois.c
@@ -1,5 +1,8 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
 #include "pico/stdlib.h"
+#include "inc/ssd1306.h"
 #include "hardware/i2c.h"
 #include "hardware/uart.h"
 
@@ -8,6 +11,17 @@
 
 #define LED_VERMELHO 13
 #define LED_VERDE 11
+
+const uint I2C_SDA = 14;
+const uint I2C_SCL = 15;
+
+// Preparar área de renderização para o display (ssd1306_width pixels por ssd1306_n_pages páginas)
+struct render_area frame_area = {
+    start_column : 0,
+    end_column : ssd1306_width - 1,
+    start_page : 0,
+    end_page : ssd1306_n_pages - 1
+};
 
 // Simula a porta AND (E ou Conjunção)
 void porta_and() {
@@ -106,6 +120,27 @@ int main()
 
     gpio_put(LED_VERMELHO, 0);
     gpio_put(LED_VERDE, 0);
+
+    // Inicialização do i2c
+    i2c_init(i2c1, ssd1306_i2c_clock * 1000);
+    gpio_set_function(I2C_SDA, GPIO_FUNC_I2C);
+    gpio_set_function(I2C_SCL, GPIO_FUNC_I2C);
+    gpio_pull_up(I2C_SDA);
+    gpio_pull_up(I2C_SCL);
+
+    uint8_t ssd[ssd1306_buffer_length];
+
+    // Processo de inicialização completo do OLED SSD1306
+    ssd1306_init();
+
+    calculate_render_area_buffer_length(&frame_area);
+
+    // zera o display inteiro
+    memset(ssd, 0, ssd1306_buffer_length);
+    render_on_display(ssd, &frame_area);
+
+    ssd1306_draw_string(ssd, 80, 80, "Bem-vindos!");
+    render_on_display(ssd, &frame_area);
 
     while (true) {
         porta_xnor();
